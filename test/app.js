44878b1a2c98daed0f34cb0ed7bf19eaa131eded
kano65 <74334160+kano65@users.noreply.github.com>
2020-12-29 23:13:01 +0900

    Add files via upload

---
+++ test/app.js
@@ -0,0 +1,10 @@
+const question = 'ゲーム史上、最も売れたゲーム機は次の内どれ？';
+const answers =[
+  'スーパーファミコン',
+  'プレイステーション２',
+  'ニンテンドースイッチ',
+  'ニンテンドーDS'
+] ;
+const correct = 'ニンテンドーDS';
+
+document.getElementsById('js-question').textContent = 'question';
